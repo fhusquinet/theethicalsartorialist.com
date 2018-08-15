08e876bc2a8dcacdb96fccc0aeefaca368d2f080
Florian Husquinet <florian.husquinet@deegital.Be>
2018-08-15 11:33:48 +0200

    💄 BaguetteBox icons

--- resources/views/_shortcodes/gallery-image.blade.php
+++ resources/views/_shortcodes/gallery-image.blade.php
@@ -1,3 +1,11 @@
-<a class="{{ $class ?? '' }}" href="{{ $image->getUrl('x-large') }}" data-caption="{{ $caption ?? '' }}">
+<a
+    class="{{ $class ?? '' }}"
+    href="{{ $image->getUrl('x-large') }}"
+    data-at-450="{{ $image->getUrl('small') }}"
+    data-at-800="{{ $image->getUrl('medium') }}"
+    data-at-1366="{{ $image->getUrl('large') }}"
+    data-at-1920="{{ $image->getUrl('x-large') }}"
+    data-caption="{{ $caption ?? '' }}"
+>
     [image id="{{ $image->id }}" class="{{ $imageClass ?? '' }}"]
 </a>
