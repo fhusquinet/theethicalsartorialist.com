576805f8cf9a8cc8d5dd9b3e71200e44936bfc18
Florian Husquinet <florian.husquinet@deegital.Be>
2018-08-15 10:57:43 +0200

    💄 Off canvas navigation

--- resources/views/_layouts/off-canvas-navigation.blade.php
+++ resources/views/_layouts/off-canvas-navigation.blade.php
@@ -4,14 +4,14 @@
             <img src="{{ asset('icons/close--white.svg') }}" alt="Close Menu Icon" class="opacity-75 hover:opacity-100 h-8 w-8 transition--fast" />
         </a>
     </div>
-    <div class="container">
-        <div class="absolute pin-center z-50">
+    <div class="absolute pin-center-y pin-l pin-r z-50">
+        <div class="container px-12 max-w-md">
             <ul class="list-reset">
-                <li><a href="" class="text-center text-grey-lightest hover:text-primary relative transition--fast text-4xl py-2 px-4 my-1 no-underline inline-block">Link 1</a></li>
-                <li><a href="" class="text-center text-grey-lightest hover:text-primary relative transition--fast text-4xl py-2 px-4 my-1 no-underline inline-block">Link 2</a></li>
-                <li><a href="" class="text-center text-grey-lightest hover:text-primary relative transition--fast text-4xl py-2 px-4 my-1 no-underline inline-block">Link 3</a></li>
-                <li><a href="" class="text-center text-grey-lightest hover:text-primary relative transition--fast text-4xl py-2 px-4 my-1 no-underline inline-block">Link 4</a></li>
-                <li><a href="" class="text-center text-grey-lightest hover:text-primary relative transition--fast text-4xl py-2 px-4 my-1 no-underline inline-block">Link 5</a></li>
+                <li><a href="{{ route('homepage') }}" class="text-grey-lightest hover:text-primary relative transition--fast text-2xl md:text-4xl font-normal py-2 px-4 my-1 no-underline block md:inline-block">Home</a></li>
+                <li><a href="{{ route('articles.index') }}" class="text-grey-lightest hover:text-primary relative transition--fast text-2xl md:text-4xl font-normal py-2 px-4 my-1 no-underline block md:inline-block">Latest Articles</a></li>
+                <li><a href="" class="text-grey-lightest hover:text-primary relative transition--fast text-2xl md:text-4xl font-normal py-2 px-4 my-1 no-underline block md:inline-block">Why buying ethical?</a></li>
+                <li><a href="" class="text-grey-lightest hover:text-primary relative transition--fast text-2xl md:text-4xl font-normal py-2 px-4 my-1 no-underline block md:inline-block">About</a></li>
+                <li><a href="" class="text-grey-lightest hover:text-primary relative transition--fast text-2xl md:text-4xl font-normal py-2 px-4 my-1 no-underline block md:inline-block">Contact</a></li>
             </ul>
         </div>
     </div>
