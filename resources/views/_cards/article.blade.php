ced2b01e9a1ba26630634895bbf2417c17723aaa
Florian Husquinet <florian.husquinet@deegital.Be>
2018-08-15 11:34:24 +0200

    💄 Article card date

--- resources/views/_cards/article.blade.php
+++ resources/views/_cards/article.blade.php
@@ -18,10 +18,10 @@
             <h2 class="card__content__title font-normal text-grey-dark text-xl mb-2 group-hover:text-grey-darker transition--fast relative">
                 {{ $article->title }}
             </h2>
-            <p class="font-thin text-grey-dark text-base md:text-sm mb-4 leading-normal">
+            <p class="font-thin text-grey-dark text-base md:text-sm mb-6 leading-normal">
                 {{ $article->preview_text }} <a href="{{ $article->getUrl() }}" class="ml-2 inline-block no-underline text-primary"><span class="inline-block pb-05 border-b border-primary">Read more</span> ></a>
             </p>
-            <p class="font-thin text-grey-light mb-4 text-sm md:text-xs">
+            <p class="font-thin text-grey mb-4 text-sm md:text-xs">
                 {{ $article->date() }}
             </p>
         </div>
