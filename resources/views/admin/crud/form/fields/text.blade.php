e5178828591521c464dadbe81bd1ad2f96f55913
Florian Husquinet <florian.husquinet@deegital.Be>
2018-09-23 14:28:59 +0200

    🐛 Admin text field fallback

--- resources/views/admin/crud/form/fields/text.blade.php
+++ resources/views/admin/crud/form/fields/text.blade.php
@@ -3,5 +3,5 @@
     name="{{ $attribute }}"
     class="border border-grey py-4 px-6 text-base font-thin text-grey rounded-lg focus:outline-none w-full max-w-sm"
     placeholder="{{ $field['placeholder'] ?? $field['label'] }}"
-    value="{{ $field['value'] ?? $model->{$attribute} }}"
+    value="{{ $field['value'] ?? $model->{$attribute} ?? '' }}"
 />
